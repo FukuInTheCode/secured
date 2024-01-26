90c859c3c8e57d0b876ac319a087f67c7605c68f
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:31:07 +0100

    [~] Update.

--- src/hash_function.c
+++ src/hash_function.c
@@ -9,5 +9,17 @@
 
 int hash(char *key, int len)
 {
-    return 0;
+    int hash_key = 112;
+    long long int o = 1;
+    int j = 1;
+
+    if (!key || len < 1)
+        return 0;
+    for (int i = 0; key[i] != '\0'; i++) {
+        hash_key += key[i] * key[i] * o * o * i * i;
+        o *= key[i] * j * hash_key;
+        o += len * my_strlen(key) * key[i];
+        j *= -1;
+    }
+    return hash_key;
 }
