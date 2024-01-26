af40537caaaebed9d268296a7542ef00e1cc27a4
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:28:58 +0100

    [~] Merge: Segfault resolve
    [~] Segfault resolve

--- src/ht_insert.c
+++ src/ht_insert.c
@@ -7,7 +7,32 @@
 
 #include "my.h"
 
+static int insert_el(info_t *info, char *key, char *value, int hashed_key)
+{
+    for (; info->next; info = info->next);
+    info->next = malloc(sizeof(hashtable_t));
+    if (!info->next)
+        return 84;
+    info->next->key = my_strdup(key);
+    info->next->value = my_strdup(value);
+    info->next->key_hashed = hashed_key;
+    info->next->next = NULL;
+    return 0;
+}
+
 int ht_insert(hashtable_t *ht, char *key, char *value)
 {
-    return 84;
+    int hashed_key = 0;
+    int id = 0;
+
+    if (!ht)
+        return 84;
+    hashed_key = ht->hash(key, ht->len);
+    id = hashed_key % ht->len;
+    if (ht->table[id].key)
+        return insert_el(ht->table + id, key, value, hashed_key);
+    ht->table[id].key = my_strdup(key);
+    ht->table[id].value = my_strdup(value);
+    ht->table[id].key_hashed = hashed_key;
+    return 0;
 }
