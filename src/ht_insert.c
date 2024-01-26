0bffcf81f1dec7d7ab909186245d87583db581e6
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:30:31 +0100

    [~] Change in the inseting to the start and not the end

--- src/ht_insert.c
+++ src/ht_insert.c
@@ -5,6 +5,7 @@
 ** ht_insert.c
 */
 
+#include "hashtable.h"
 #include "my.h"
 
 static int change_el(info_t *info, char *value)
@@ -14,18 +15,23 @@ static int change_el(info_t *info, char *value)
     return 84 * !info->value;
 }
 
-static int insert_el(info_t *info, char *key, char *value, int hashed_key)
+static int insert_el(info_t *head, char *key, char *value, int hashed_key)
 {
-    for (; info->next; info = info->next)
+    info_t *tmp = NULL;
+
+    for (info_t *info = head; info->next; info = info->next)
         if (!my_strcmp(info->next->key, key))
             return change_el(info->next, value);
-    info->next = malloc(sizeof(info_t));
-    if (!info->next)
+    tmp = malloc(sizeof(info_t));
+    if (!tmp)
         return 84;
-    info->next->key = my_strdup(key);
-    info->next->value = my_strdup(value);
-    info->next->key_hashed = hashed_key;
-    info->next->next = NULL;
+    tmp->next = head->next;
+    tmp->key = head->key;
+    tmp->value = head->value;
+    tmp->key_hashed = head->key_hashed;
+    head->key = my_strdup(key);
+    head->value = my_strdup(value);
+    head->key_hashed = hashed_key;
     return 0;
 }
 
@@ -34,7 +40,7 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
     int hashed_key = 0;
     int id = 0;
 
-    if (!ht || !key || !value || !ht->table || !ht->hash)
+    if (!ht || !key || !value || !ht->table)
         return 84;
     hashed_key = ht->hash(key, ht->len);
     id = hashed_key % ht->len;
