193242669f46b365fb31dd2e49c392a62495cd8d
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:28:28 +0100

    [~] add ht_dump print

--- src/ht_dump.c
+++ src/ht_dump.c
@@ -9,6 +9,11 @@
 
 void ht_dump(hashtable_t *ht)
 {
-    mini_printf("PASSED\n");
+    for (int i = 0; i != ht->len; i++) {
+        mini_printf("[%d]:\n"), i;
+        for (info_t *j = ht->table; j != NULL; j = j->next) {
+            mini_printf("> %d - %s\n", j->key_hashed, j->value);
+        }
+    }
     return;
 }
