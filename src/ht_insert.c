d178cffd152f742f594ceaf6b0fb6da4ac290c4e
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:28:35 +0100

    [~] modifie the ht_insert to pass the correct key

--- src/ht_insert.c
+++ src/ht_insert.c
@@ -23,14 +23,16 @@ static int insert_el(info_t *info, char *key, char *value, int hashed_value)
 int ht_insert(hashtable_t *ht, char *key, char *value)
 {
     int hashed_key = 0;
+    int id = 0;
 
     if (!ht)
         return 84;
-    hashed_key = ht->hash(key, ht->len) % ht->len;
-    if (ht->table[hashed_value].key)
-        return insert_el(ht->table + hashed_key, key, value, hashed_key);
-    ht->table[hashed_key].key = my_strdup(key);
-    ht->table[hashed_key].value = my_strdup(value);
-    ht->table[hashed_key].key_hashed = hashed_key;
+    hashed_key = ht->hash(key, ht->len);
+    id = hashed_key % ht->len;
+    if (ht->table[id].key)
+        return insert_el(ht->table + id, key, value, hashed_key);
+    ht->table[id].key = my_strdup(key);
+    ht->table[id].value = my_strdup(value);
+    ht->table[id].key_hashed = hashed_key;
     return 0;
 }
