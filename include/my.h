67b29597d373612a75d9c0288e0d0cbce260a33e
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:27:26 +0100

    [+] Initialize the repository

---
+++ include/my.h
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+    #define _GNU_SOURCE
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <stdarg.h>
+    #include <errno.h>
+    #include <math.h>
+    #include <stdlib.h>
+    #include <string.h>
+
+    #include <criterion/criterion.h>
+    #include <criterion/redirect.h>
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Config.h>
+    #include <SFML/GPUPreference.h>
+    #include <SFML/Network.h>
+    #include <SFML/OpenGL.h>
+    #include <SFML/System.h>
+    #include <SFML/Window.h>
+
+#endif
