f16fa977f8dd3e2b7e950ebe9e64c4431d551474
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:30:06 +0100

    [~] fix id when is negative

--- tests/test_main.c
+++ tests/test_main.c
@@ -9,14 +9,11 @@
 
 int main(void)
 {
-    hashtable_t *ht = new_hashtable(&hash, 4);
-    ht_insert(ht, "Vision", "./Documents/Tournament/Modules/Vision") ;
-    ht_insert(ht, "Kratos", "./Trash/Hollidays_Pics/.secret_folder/kratos.ai") ;
+    hashtable_t * ht = new_hashtable (&hash, 4);
+    ht_insert(ht, "Vision", "./Documents/Tournament/Modules/Vision");
+    ht_insert(ht, "Kratos", "./Trash/Hollidays_Pics/.secret_folder/kratos.ai");
     ht_insert(ht, "<3", "+33 6 31 45 61 23 71");
-    ht_insert(ht, "nonono", "+33 6 31 45 61 23 72");
-    ht_insert(ht, "hellopipo", "+33 6 31 45 61 23 73");
+    ht_insert(ht, "</3", "+33 7 51 49 01 38 11");
     ht_dump(ht);
-    printf("Looking for \"%s\": \"%s\"\n", "Kratos", ht_search(ht, "nonono"));
-    printf("%d\n", hash("nonono", 4) % 4);
     return 0;
 }
