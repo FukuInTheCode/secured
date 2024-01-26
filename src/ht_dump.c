880015807efc0c2f1f7374a770344b6a1cb37752
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:29:09 +0100

    [~] Merge: Segfault fix in the malloc of insert_el()
    [~] Segfault fix in the malloc of insert_el()

--- src/ht_dump.c
+++ src/ht_dump.c
@@ -9,5 +9,10 @@
 
 void ht_dump(hashtable_t *ht)
 {
-    return;
+    for (int i = 0; i != ht->len; i++) {
+        mini_printf("[%d]:\n", i);
+        for (info_t *j = ht->table; j != NULL; j = j->next) {
+            mini_printf("> %d - %s\n", j->key_hashed, j->value);
+        }
+    }
 }
