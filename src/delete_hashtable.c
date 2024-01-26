f41931955b98c8749ce9589376d6a8d5cdc471a7
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:30:16 +0100

    [+] Add the delete_hashtable

---
+++ src/delete_hashtable.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2024
+** delete_hashtable.c
+** File description:
+** delete_hashtable.c
+*/
+
+#include "my.h"
+
+static int delete_list(info_t *el)
+{
+    info_t *previous = el;
+
+    for (info_t *tmp = el; tmp; tmp = tmp->next) {
+        free(tmp->key);
+        free(tmp->value);
+        free(previous);
+        if (tmp != el)
+            previous = tmp;
+    }
+    return 0;
+}
+
+void delete_hashtable(hashtable_t *ht)
+{
+    if (!ht)
+        return;
+    if (!ht->table)
+        return;
+    for (int i = 0; i < ht->len; i++)
+        delete_list(ht->table + i);
+}
