eb4d4ddf7e05cb11b1dbe3043fef8bca20135e86
Tom C. <tom.carlier@epitech.eu>
2024-01-26 14:29:54 +0100

    [~] remove useless boucle

--- src/ht_dump.c
+++ src/ht_dump.c
@@ -7,13 +7,11 @@
 
 #include "my.h"
 
-void print_in_index(hashtable_t *ht, int i)
+void print_in_index(hashtable_t *ht, int i, info_t *j)
 {
-    for (info_t *j = ht->table + i; j != NULL; j = j->next) {
-        if (!j->value)
-            continue;
-        mini_printf("> %d - %s\n", j->key_hashed, j->value);
-    }
+    if (!j->value)
+        return;
+    mini_printf("> %d - %s\n", j->key_hashed, j->value);
 }
 
 void ht_dump(hashtable_t *ht)
@@ -21,7 +19,7 @@ void ht_dump(hashtable_t *ht)
     for (int i = 0; i != ht->len; i++) {
         mini_printf("[%d]:\n", i);
         for (info_t *j = ht->table + i; j != NULL; j = j->next) {
-            print_in_index(ht, i);
+            print_in_index(ht, i, j);
         }
     }
 }
